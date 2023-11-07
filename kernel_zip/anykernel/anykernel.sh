d34d2f9cf36bafef6c45376c0f12bbd7041ff24b
kevios12 <kerschbaumer.kevin2@gmail.com>
2023-11-07 23:47:05 +0100

    kernel: add AnyKernel3 for packing and flashing manually

---
+++ kernel_zip/anykernel/anykernel.sh
@@ -0,0 +1,43 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Eureka Kernel by Eureka Team
+do.devicecheck=0
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+## AnyKernel install
+#Method 1:
+dump_boot;
+ui_print "- Installing 4.9 kernel";
+write_boot;
+ui_print "- Installation finished successfully";
+ui_print " ";
+## end install
+
